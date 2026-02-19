43ec94c27d68a13b30727267eea227b1d77f8f14
Jakob Schoeberle <jrschoeberle@gmail.com>
2026-02-18 20:51:58 -0600

    Added Read Switch Code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -66,6 +68,8 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousPeriodic() {}
 
+  DigitalInput limitSwitch = new DigitalInput(0);
+
   @Override
   public void teleopInit() {
     // This makes sure that the autonomous stops running when
@@ -75,11 +79,18 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+
+
+
   }
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+
+    boolean isPressed = !limitSwitch.get();
+    SmartDashboard.putBoolean("Switch Pressed", isPressed);
+  }
 
   @Override
   public void testInit() {
